80461664a748117a3ec8524a79bcac22b02dc8ad
abdul diallo <rahimi.diallo96@gmail.com>
2020-02-20 11:01:49 -0500

    link the button and the labels to IBAction and IBOutlets

--- TweetSentiment/ViewController.swift
+++ TweetSentiment/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sentimentLabel: UILabel!
+    @IBOutlet weak var inputText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func checkSentiment(_ sender: UIButton) {
+    }
+    
 }
 
