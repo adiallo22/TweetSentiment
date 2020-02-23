6746c87d1dfc50f2edae71f40a691714bc9cde39
abdul diallo <rahimi.diallo96@gmail.com>
2020-02-23 09:03:15 -0500

    add the swiftyJSON library to parse the result received from the api request

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'TweetSentiment' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TweetSentiment
+pod 'SwiftyJSON'
+
+end
--- TweetSentiment/ViewController.swift
+++ TweetSentiment/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -16,12 +18,29 @@ class ViewController: UIViewController {
     
     let swifter = Swifter(consumerKey: "4KEjNbACHcM1lKTqojq4PeoE6", consumerSecret: "aguhLFBeJd7mbbUAJ2vvXDCASBAqjZwdwuCRNzkreDtPI4Go9T")
     
+    let model = sentimentClassifier()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let output = try! model.prediction(text: "@google is a nice place to work")
+        sentimentLabel.text = output.label
     }
 
     @IBAction func checkSentiment(_ sender: UIButton) {
+        if let inputed = sentimentLabel.text {
+            swifter.searchTweet(using: "\(inputed)", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+                var tweets = [String]()
+                for i in 0...99 {
+                    if let tweet = results[i]["full_text"].string {
+                        tweets.append(tweet)
+                    }
+                }
+                print(tweets)
+            }) { (error) in
+                print("error - \(error)")
+            }
+        }
     }
     
 }
